f7dca4ec1fe6ccec6b08d83c4245f023b4c08b15
David Herrera <davidfherrerar@gmail.com>
2015-10-19 15:10:31 -0400

    Adding the forms controller:

--- 8fractals/app/scripts/controllers/FormsController.js
+++ 8fractals/app/scripts/controllers/FormsController.js
@@ -1,7 +1,23 @@
 angular.module('8fractals.controllers').
-controller('FormsController',['$scope', '$timeout','$rootScope', '$state', function($scope, $timeout,$rootScope,$state){
+controller('FormsController',['$scope', '$timeout','$rootScope', '$state', function($http, $scope, $timeout,$rootScope,$state){
   $rootScope.showFractalsMenu=false;
   $scope.login=function(){
-    $state.go('tab.dash');
+
+    var req = {
+ method: 'POST',
+ url: 'localhost:8888/fractal-beta/login.php',
+ headers: {
+   'Content-Type': undefined
+ },
+ data: { username:$scope.loginUser.username,
+          password:$scope.loginUser.password}
+}
+    $http(req).then(function(data){
+      console.log(data);
+      $state.go('tab.dash');
+    }, function(error){
+      console.log(error);
+    });
+
   };
 }]);
